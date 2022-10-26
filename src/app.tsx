f7ad1e754537dd4bcddd030b1f8cd57a4317ec09
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2022-10-26 19:16:01 +0200

    initial ts+webpack+react+mui project

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Button from '@mui/material/Button';
+
+export function App() {
+  return <>
+    <CssBaseline enableColorScheme />
+    <Button variant="contained">Hello World</Button>
+  </>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
